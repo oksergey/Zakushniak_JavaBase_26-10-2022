ada925e918fe98b74cc854ae0b8a24880ac8cf32
Zakushniak Sergii <ok.sergey@gmail.com>
2022-11-29 13:17:47 +0200

    домашка 6

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson5/Main.java
@@ -0,0 +1,43 @@
+package com.hillel.zakushniak.lessons.lesson5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // ДЗ 6. Counter Strike
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Введіть назву першої команди");
+        String team1Name = scanner.nextLine();
+        System.out.println("використовуючи Enter, введіть кількість очок кожного з п'яти ігроків");
+        int player1Team1 = scanner.nextInt();
+        int player2Team1 = scanner.nextInt();
+        int player3Team1 = scanner.nextInt();
+        int player4Team1 = scanner.nextInt();
+        int player5Team1 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.println("Введіть назву другої команди");
+        String team2Name = scanner.nextLine();
+        System.out.println("використовуючи Enter, введіть кількість очок кожного з п'яти ігроків");
+        int player1Team2 = scanner.nextInt();
+        int player2Team2 = scanner.nextInt();
+        int player3Team2 = scanner.nextInt();
+        int player4Team2 = scanner.nextInt();
+        int player5Team2 = scanner.nextInt();
+        scanner.nextLine();
+
+        int averageFragsTeam1 = (player1Team1 + player2Team1 + player3Team1 + player4Team1 + player5Team1) / 5;
+        int averageFragsTeam2 = (player1Team2 + player2Team2 + player3Team2 + player4Team2 + player5Team2) / 5;
+
+        if (averageFragsTeam1 > averageFragsTeam2) {
+            System.out.println("Перемогла команда " + team1Name + " набрала " + averageFragsTeam1 + " очків");
+        } else if (averageFragsTeam1 < averageFragsTeam2) {
+            System.out.println("Перемогла команда " + team2Name + " набрала " + averageFragsTeam2 + " очків");
+        } else {
+            System.out.println("Маємо бойову нічию, обидві команди набрали по " + averageFragsTeam1 + " очків");
+        }
+    }
+}
